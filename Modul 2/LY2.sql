74aa0f01da7a75382d75bcd1d6feaa55db29e6cc
Luxy <146051503+lusy17@users.noreply.github.com>
2025-04-24 10:21:52 +0800

    Update modul2.sql

--- Modul 2/LY2.sql
+++ Modul 2/LY2.sql
@@ -1,54 +1,59 @@
--- Nama : Luis
--- Nim  : 23241048
+-- Nama : Luisy
+-- NIM : 23241048
 -- Kelas : B
--- Modul : 2 (select)
+-- Modul : 2 (SELECT)
 
 -- Membuat database
 CREATE DATABASE pti_mart;
 
--- menggunakan database
-USE pti_mart;     
+-- Menggunakan database
+USE pti_mart;
 
--- cek kolom dalam setiap tabel
+-- Cek kolom dalam setiap tabel
 DESCRIBE ms_pelanggan;
 DESCRIBE ms_produk;
 DESCRIBE tr_penjualan;
 
--- select 
--- menggunakan select untuk 1 tabel
--- menggambil kolom nama produk dari tabel produk
-SELECT nama_produk From ms_produk;
+-- SELECT 
+-- Menggunakan SELECT untuk 1 Table
+-- Mengambil kolom nama produk dari table produk
+SELECT nama_produk FROM ms_produk;
 
+-- Mengambil lebih dari satu kolom dari sebuah table
+SELECT nama_produk, harga FROM ms_produk;
 
--- Mengambil lebih dari 1 kolom dari sebuah tabel
- SELECT nama_produk, harga FROM ms_produk;
- 
- -- mengambil semua kolom dari sebuah tabel
- SELECT * FROM ms_produk;
- 
- -- Latihan 
- -- mengambil kode produk dan nama produk dari tabel produk
- SELECT kode_ produk, nama_produk FROM ms_produk;
- 
- -- mengambil seluruh kolom dari tabel penjual
- SELECT * FROM tr_penjualan;
+-- Mengambil semua kolom dari sebuah table 
+SELECT * FROM ms_produk;
 
--- prefix dan alias
+-- LATIHAN
+-- Mengambil kode produk dan nama produk dari table produk
+SELECT kode_produk, nama_produk FROM ms_produk;
+
+-- Mengambil seluruh kolom dari table penjualan
+SELECT * FROM ms_pelanggan;
+
+-- PREFIX dan ALIAS
 -- PREFIX, bagian objek database yang hirarkinya lebih tinggi
+-- Menggunakan PREFIX nama table untuk kolom nama_produk dari table produk
 SELECT ms_produk.nama_produk FROM ms_produk;
 
--- menggunakan nama database dan tabel sebagai prefix untuk kolom nama_produk
-SELECT pti_mrt.ms_produk FROM ms_produk;  
+-- Menggunakan nama database dan table sebagai prefix untuk kolom nama_produk
+SELECT pti_mart.ms_produk.nama_produk FROM ms_produk;
 
--- ALIAS, nama sementara untuk objek database\
--- mengganti sementara kolom nama_produk dengan ALIAS
+
+-- ALTAS, nama sementara untuk objek database
+-- Mengambil sementara kolom nama_produk dengan ALIAS
 SELECT nama_produk AS np FROM ms_produk;
-SELECT nama_Produk np FROM ms_produk;
+SELECT nama_produk np FROM ms_produk;
 
--- mengganti sementara tabel dengan alias
-SELECT nama_produk FROM ms_produk AS msp;
+-- Mengganti sementara table dengan ALTAS
+SELECT nama_produk FROM  ms_produk AS msp;
 
--- kombinasikan prefix dan alias
-SELECT msp_nama_produk FROM  
+-- Kombinasikan PREFIX dan ALIAS
 
-                                                                                                                                                      
+
+-- CASE 1
+SELECT nama_pelanggan,alamat FROM ms_pelanggan;
+
+-- CASE 2
+SELECT nama_produk,harga FROM ms_produk;
